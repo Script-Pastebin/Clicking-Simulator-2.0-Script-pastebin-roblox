76fa566e05f30a853dd5db2beabf6b2ea47c9c36
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 06:58:59 +0100

    commit zero

---
+++ Clicking Simulator 2.0 Script pastebin roblox Clicking Simulator 2.0 .lua
@@ -0,0 +1,9 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Christmas8335/roblox/master/ClickingSimulator2',true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+This is a practical script.It has: Fast click, Open 3 eggs, auto farm and more!
+]]
